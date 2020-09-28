293a3f811642c51b0e99f4a01a3e24f199a77406
eloiguerra <joseeloi.juninho@gmail.com>
2020-09-28 19:46:39 -0300

    Add read

--- src/controllers/FriendController.js
+++ src/controllers/FriendController.js
@@ -3,5 +3,18 @@ const Friend = require('../models/FriendModel');
 module.exports = {
   async store(req, res){
     const {email} = req.body;
-  }
+  },
+
+  async read(req, res){
+    try{
+      const {id} = req.params;
+
+      user = await Friend.findById(id);
+
+      return res.status(201).send(user);
+    }
+    catch(err){
+      console.log(err);
+    }
+  },
 }
