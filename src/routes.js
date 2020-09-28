43420dde3f78a9dc8fb22e6cdc99677486d44869
eloiguerra <joseeloi.juninho@gmail.com>
2020-09-28 19:45:52 -0300

    Add routes to images upload and visit profiles

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,31 @@
 const express = require('express');
 const routes = express.Router();
 
+const multer = require('multer');
+const multerConfig = require('./config/multer');
+
 const UserController = require('./controllers/UserController');
 const FriendController = require('./controllers/FriendController');
+const ImageController = require('./controllers/ImageController');
 
 const auth = require('./middlewares/auth');
 
 routes.post('/register', UserController.store);
 routes.post('/login', UserController.login);
-routes.get('/users/:full_name', auth, UserController.read);
+routes.get('/users', auth, UserController.read);
+routes.get('/users/:full_name', auth, UserController.readByName);
+routes.put('/users', auth, UserController.update);
 
-routes.post('/friend/:id', auth, FriendController.store);
+routes.get('/visit/:id', auth, UserController.readById);
+
+// routes.get('/friends/:id', auth, FriendController.read);
+// routes.post('/friends/:id', auth, FriendController.store);
+
+routes.post('/postProfilePhoto',
+  multer(multerConfig).single('file'),
+  ImageController.profileStore
+);
+
+
 
 module.exports = routes;
