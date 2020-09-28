e8cb5acfd42abee51005fddf4f6c95c15af89de9
eloiguerra <joseeloi.juninho@gmail.com>
2020-09-28 19:46:06 -0300

    Add static route to images

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 require('dotenv/config');
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -8,6 +9,11 @@ const routes = require('./routes');
 
 app.use(cors());
 app.use(express.json());
-app.use('/', routes)
+app.use(express.urlencoded({ extended: true}));
+app.use(
+  '/files',
+  express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
+);
+app.use('/', routes);
 
 app.listen(process.env.PORT || 3333);
