d044f2f61e2e62971fa8b27e65fa113ae02e4ca9
eloiguerra <joseeloi.juninho@gmail.com>
2020-10-01 10:20:50 -0300

    Add friend and sport routes

--- src/routes.js
+++ src/routes.js
@@ -7,25 +7,30 @@ const multerConfig = require('./config/multer');
 const UserController = require('./controllers/UserController');
 const FriendController = require('./controllers/FriendController');
 const ImageController = require('./controllers/ImageController');
+const SportController = require('./controllers/SportController');
 
 const auth = require('./middlewares/auth');
 
 routes.post('/register', UserController.store);
 routes.post('/login', UserController.login);
+
 routes.get('/users', auth, UserController.read);
 routes.get('/users/:full_name', auth, UserController.readByName);
 routes.put('/users', auth, UserController.update);
 
 routes.get('/visit/:id', auth, UserController.readById);
 
-// routes.get('/friends/:id', auth, FriendController.read);
-// routes.post('/friends/:id', auth, FriendController.store);
+routes.post('/friends', auth, FriendController.store);
+routes.get('/friends/:id', auth, FriendController.read);
+routes.put('/friends', auth, FriendController.update);
+
+//Se der tempo, lembrar de fazer o login de adm pra essa rota
+routes.post('/sports', SportController.create);
+routes.get('/sports', SportController.read);
 
 routes.post('/postProfilePhoto',
   multer(multerConfig).single('file'),
   ImageController.profileStore
 );
-
-
 
 module.exports = routes;
