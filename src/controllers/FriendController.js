1754a3bd08a54bd4fd184bf1aa490d7d094c2a0f
eloiguerra <joseeloi.juninho@gmail.com>
2020-10-01 10:21:10 -0300

    Add CRU

--- src/controllers/FriendController.js
+++ src/controllers/FriendController.js
@@ -2,19 +2,59 @@ const Friend = require('../models/FriendModel');
 
 module.exports = {
   async store(req, res){
-    const {email} = req.body;
+    const user = req.user._id;
+    const friend = req.body.id;
+
+    const friendship = await Friend.findOne({user, friend});
+    if(friendship){
+      if(friendship.friend_request){
+        return res.status(400).send({message: 'Pending friend request'});
+      }
+      else{
+        return res.status(400).send({message: 'You are already friends'})
+      }
+    }
+    else {
+      const result = await Friend.create({
+        user,
+        friend,
+        friend_request: true,
+      });
+
+      return res.status(201).send(result);
+    }
   },
 
   async read(req, res){
     try{
       const {id} = req.params;
 
-      user = await Friend.findById(id);
+      const friendship = await Friend.findOne({
+        $or:[
+          {friend: id}, {user: id}
+        ]
+      });
 
-      return res.status(201).send(user);
+      return res.status(200).send(friendship);
     }
     catch(err){
       console.log(err);
     }
   },
+
+  async update(req, res){
+    try{
+      const {id} = req.body;
+
+      const friendship = await Friend.findByIdAndUpdate(
+        id,
+        {friend_request: false}
+      );
+
+      return res.status(200).send(friendship);
+    }
+    catch(err){
+      console.log(err);
+    }
+  }
 }
