021c40d15d35ff88cecd5f1741842ded8c0c9af0
eloiguerra <joseeloi.juninho@gmail.com>
2020-10-01 10:21:20 -0300

    Add CR

--- src/controllers/SportController.js
+++ src/controllers/SportController.js
@@ -3,10 +3,25 @@ const Sport = require('../models/SportModel');
 module.exports = {
   async create(req, res){
     try{
+      const {name} = req.body;
 
+      const sport = await Sport.create({name})
+
+      return res.status(201).send(sport);
     }
     catch(err){
+      console.log(err);
+    }
+  },
 
+  async read(req, res){
+    try{
+      const sports = await Sport.find();
+
+      return res.status(200).send(sports);
+    }
+    catch(err){
+      console.log(err);
     }
   }
 }
