aec1c3b431190871c7e7f3f80e0e3953cb6d2de8
eloiguerra <joseeloi.juninho@gmail.com>
2020-10-02 12:21:09 -0300

    Add publication controller

---
+++ src/controllers/PublicationController.js
@@ -0,0 +1,33 @@
+const Publication = require('../models/PublicationModel');
+
+module.exports = {
+  async create(req, res){
+    try{
+      const {_id} = req.user;
+      const {description, sport} = req.body;
+
+      const publication = await Publication.create({
+        user: _id,
+        sport: "5f75d42b3e25753e755b7970",
+        description,
+        image: null,
+      });
+
+      return res.status(201).send(publication);
+    }
+    catch(err){
+      console.log(err);
+    }
+  },
+
+  async read(req, res){
+    try{
+      const publications = await Publication.find().populate();
+
+      return res.status(200).send(publications);
+    }
+    catch(err){
+      console.log(err);
+    }
+  }
+}
