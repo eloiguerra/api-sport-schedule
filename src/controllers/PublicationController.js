f037e6e2f339fb651c5cab5cc96c8fe9c29e22ca
eloiguerra <joseeloi.juninho@gmail.com>
2020-10-19 16:18:48 -0300

    Add images in publications

--- src/controllers/PublicationController.js
+++ src/controllers/PublicationController.js
@@ -1,16 +1,17 @@
 const Publication = require('../models/PublicationModel');
+const Friend = require('../models/FriendModel');
 
 module.exports = {
   async create(req, res){
     try{
       const {_id} = req.user;
-      const {description, sport} = req.body;
+      const {description, sport, image} = req.body;
 
       const publication = await Publication.create({
         user: _id,
         sport,
         description,
-        image: null,
+        image
       });
 
       return res.status(201).send(publication);
@@ -25,6 +26,53 @@ module.exports = {
       const publications = await Publication.find().populate();
 
       return res.status(200).send(publications);
+    }
+    catch(err){
+      console.log(err);
+    }
+  },
+
+  async readByFriends(req, res){
+    try{
+      const {_id} = req.user;
+      const {page} = req.query;
+      const {limit} = req.query;
+
+      const startIndex = (page - 1) * limit;
+      const endIndex = page * limit;
+
+      let friends = [];
+
+      const friendship = await Friend.find({
+        $and:[
+          {$or:[
+            {$and: [{friend: {$ne: _id}}, {user: _id}]},
+            {$and: [{friend: _id}, {user: {$ne: _id}}]},
+          ]},
+          {friend_request: false}
+        ]
+      });
+
+      friendship.map(item => {
+        if(item.user != _id)
+          friends.push(item.user);
+        else
+          friends.push(item.friend);
+      });
+
+      const publications = await Publication.find({
+        user: {$in: friends}
+      })
+      .limit(endIndex).skip(startIndex)
+      .populate({
+        path: 'user',
+        populate: {path: 'profile_photo'}
+      })
+      .populate('sport')
+      .populate('image', 'url')
+
+      if(publications.length)
+        return res.status(200).send(publications);
     }
     catch(err){
       console.log(err);
