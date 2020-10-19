674b6fe41eafafe1493fa1347f49b7b20d7dc470
eloiguerra <joseeloi.juninho@gmail.com>
2020-10-19 19:28:12 -0300

    Removing profile photo update

--- src/controllers/ImageController.js
+++ src/controllers/ImageController.js
@@ -1,6 +1,4 @@
 const Image = require('../models/ImageModel');
-const User = require('../models/UserModel');
-
 module.exports = {
   async store(req, res){
     try{
@@ -31,33 +29,4 @@ module.exports = {
       return res.status(400).send({error: err});
     }
   },
-
-  async update(req, res){
-    try{
-      const session = await Image.startSession();
-      const {_id} = req.user;
-      const {originalname: name, size, filename: key} = req.file;
-
-      await session.withTransaction(async () => {
-        let new_photo = await Image.create({
-          name,
-          size,
-          key,
-          url: `http://localhost:3333/files/${key}`
-        });
-
-        let {profile_photo} = await User.findById(_id).select('profile_photo');
-        if(!profile_photo === "5f78f89f85cf6047a144f9fb"){
-          await findByIdAndDelete(profile_photo);
-        }
-
-        await User.findByIdAndUpdate(_id, {profile_photo: new_photo._id});
-
-        return res.status(201).send({});
-      });
-    }
-    catch(err){
-      return res.status(400).send({error: err});
-    }
-  }
 }
