787d57f393f7486f7b909d052e55477c2a3fc78b
eloiguerra <joseeloi.juninho@gmail.com>
2020-10-19 16:17:29 -0300

    Add message Model

---
+++ src/models/MessageModel.js
@@ -0,0 +1,7 @@
+// const moongose = require('../config/database');
+// const MessageSchema = require('../schemas/MessageSchema');
+
+// const Message = moongose.model('Message', MessageSchema);
+
+// module.exports = Message;
+
