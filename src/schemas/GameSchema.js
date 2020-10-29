5897c8fa24a0f8fe151950f619849c2bd6b14f09
eloiguerra <joseeloi.juninho@gmail.com>
2020-10-29 10:43:17 -0300

    Add game schema

---
+++ src/schemas/GameSchema.js
@@ -0,0 +1,48 @@
+const moongose = require('mongoose');
+const Schema = moongose.Schema;
+
+const GameSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  lat: {
+    type: Number,
+    required: true
+  },
+  lng: {
+    type: Number,
+    required: true
+  },
+  sport: {
+    type: moongose.Schema.Types.ObjectId,
+    ref: 'Sport',
+    required: true
+  },
+  owner: {
+    type: moongose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  players: [{
+    type: moongose.Schema.Types.ObjectId,
+    ref: 'User',
+  }],
+  created_at: {
+    type: Date,
+    default: Date.now
+  },
+  updated_at: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = GameSchema;
