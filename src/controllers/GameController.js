721162cece246a5005406981c200dedeaba2e7ab
eloiguerra <joseeloi.juninho@gmail.com>
2020-10-29 10:43:04 -0300

    Add game controller

---
+++ src/controllers/GameController.js
@@ -0,0 +1,40 @@
+const Game = require('../models/GameModel');
+
+module.exports = {
+  async create(req, res){
+    try{
+      const {
+        name, date, description,
+        sport, lat, lng
+      } = req.body;
+
+      const game = await Game.create({
+        owner: req.user._id,
+        name,
+        description,
+        date,
+        sport,
+        lat,
+        lng
+      });
+
+      return res.status(201).send(game);
+    }
+    catch(err){
+      return res.send(err);
+    }
+  },
+
+  async read(req, res){
+    try{
+      const games = await Game.find()
+      .populate('sport')
+      .populate('owner', 'full_name');
+
+      return res.status(200).send(games);
+    }
+    catch(err){
+      return res.send(err);
+    }
+  }
+}
